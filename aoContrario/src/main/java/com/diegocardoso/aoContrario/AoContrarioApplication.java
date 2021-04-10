70cb30257da4986d1804bca8629a371595ef3dcf
Diego Cardoso <vicioonline@gmail.com>
2021-04-09 21:26:54 -0300

    inicial

---
+++ aoContrario/src/main/java/com/diegocardoso/aoContrario/AoContrarioApplication.java
@@ -0,0 +1,13 @@
+package com.diegocardoso.aoContrario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoContrarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoContrarioApplication.class, args);
+	}
+
+}
---
+++ aoContrario/src/test/java/com/diegocardoso/aoContrario/AoContrarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegocardoso.aoContrario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoContrarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
